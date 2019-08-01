3fac85212554e5950b157fea12993f016355af4e
luoran0216 <53433600+luoran0216@users.noreply.github.com>
2019-08-01 19:26:09 +0800

    Update test_001.py

--- scripts/test_001.py
+++ scripts/test_001.py
@@ -3,7 +3,7 @@ import allure, pytest
 
 class Test_001:
 
-    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)  # 阻塞性bug
+    #@pytest.allure.severity(pytest.allure.severity_level.BLOCKER)  # 阻塞性bug
     @allure.step(title="这是测试步骤")
     def test_001(self):
         allure.attach("描述", "我是描述的具体内容")
